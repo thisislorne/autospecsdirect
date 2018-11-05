adb7a02305f06669eb84069824db0092a974eba4
JordanFisherMan <jordan@groovymedia.com>
2018-11-06 09:45:47 +1300

    rename url correctly

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
-server 'awoolo.co.nz', user: 'deploy', roles: %w(web app db)
+server 'awoolo.com', user: 'deploy', roles: %w(web app db)
 
 set :rails_env, 'production'
-set :nginx_domains, 'awoolo.co.nz www.awoolo.co.nz'
+set :nginx_domains, 'awoolo.com www.awoolo.com'
 set :branch, :master
-set :nginx_ssl_certificate_path, '/etc/letsencrypt/live/awoolo.co.nz'
-set :nginx_ssl_certificate_key_path, '/etc/letsencrypt/live/awoolo.co.nz'
+set :nginx_ssl_certificate_path, '/etc/letsencrypt/live/awoolo.com'
+set :nginx_ssl_certificate_key_path, '/etc/letsencrypt/live/awoolo.com'
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,7 @@
-server 'staging.awoolo.co.nz', user: 'deploy', roles: %w(web app db)
+server 'staging.awoolo.com', user: 'deploy', roles: %w(web app db)
 
 set :rails_env, 'staging'
-set :nginx_domains, 'staging.awoolo.co.nz'
+set :nginx_domains, 'staging.awoolo.com'
 set :branch, :staging
-set :nginx_ssl_certificate_path, '/etc/letsencrypt/live/staging.awoolo.co.nz'
-set :nginx_ssl_certificate_key_path, '/etc/letsencrypt/live/staging.awoolo.co.nz'
+set :nginx_ssl_certificate_path, '/etc/letsencrypt/live/staging.awoolo.com'
+set :nginx_ssl_certificate_key_path, '/etc/letsencrypt/live/staging.awoolo.com'
