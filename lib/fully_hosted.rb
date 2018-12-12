12ca638f9d49f8e55679076861294bcebce44861
JordanFisherMan <jordan@groovymedia.com>
2018-12-12 13:37:01 +1300

    encode url to ensure it can always be parsed

--- lib/fully_hosted.rb
+++ lib/fully_hosted.rb
@@ -2,7 +2,7 @@ module FullyHosted
   def FullyHosted.get_link(options)
     raise 'No options object provided' unless options
     raise 'Missing injection params' unless options[:params]
-  
+
     encrypted = encrypt(URI.encode_www_form(options[:params]), FullyHosted::PUBLIC_KEY)
     query_string = {
       ic_user_id: options[:IC_USER_ID],
@@ -23,6 +23,7 @@ module FullyHosted
     req['X-Forwarded-For'] = options[:x_forwarded_for] if options[:x_forwarded_for]
     req.set_form_data encrypted
     link = http.request(req).body
+    link = URI.encode(link)
     return link if URI.parse(link).hostname
     "#{FullyHosted::DEFAULT_DOMAIN}#{link[1..-1]}"
   end
