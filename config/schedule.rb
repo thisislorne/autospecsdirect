4800dbd4e2bdea855d98be79579dae379f66da1d
Lorne Davies <thisislorne@gmail.com>
2019-04-17 11:26:03 +1200

    cron adjust

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,7 +6,7 @@ set :output, {
   standard: Rails.root.join('log', "#{@environment}_cron.log"),
   error: Rails.root.join('log', "#{@environment}_cron_error.log")
 }
-every 1.hour do
+every :hour do
   thor 'genius:optimise_keywords'
 end
 
