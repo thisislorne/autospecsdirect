44b442861d8864aaf06c04a52e6c7d0ad79369d4
Lorne Davies <thisislorne@gmail.com>
2019-07-02 12:01:00 +1200

    staging db

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,7 @@
-server 'staging.searchbe.com', user: 'deploy', roles: %w(web app db)
+server '34.232.222.53', user: 'deploy', roles: %w(web app db)
 
 set :rails_env, 'staging'
 set :nginx_domains, 'staging.searchbe.com'
-set :branch, :staging
+set :branch, :rewrite
 set :nginx_ssl_certificate_path, '/etc/letsencrypt/live/staging.searchbe.com'
 set :nginx_ssl_certificate_key_path, '/etc/letsencrypt/live/staging.searchbe.com'
