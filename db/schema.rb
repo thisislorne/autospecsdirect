6dac3df910e9354bc5e39b8b045e1016a5d72794
Lorne Davies <thisislorne@gmail.com>
2020-05-13 10:19:22 +1200

    creds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_17_002919) do
+ActiveRecord::Schema.define(version: 2020_04_08_091829) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2020_03_17_002919) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "adgroup_ids", default: [], array: true
+    t.integer "p_val", default: 2
     t.index ["slug"], name: "index_searches_on_slug"
   end
 
